98162450972b9cdf79ddde2559d1bf8431ec0cff
Simon Ringuette <sringuette@trisotech.com>
2022-12-07 13:35:53 -0500

    Re-introduced the bpmn-envelope-decorator-bounds.xsd with an attribute group

--- test-validation-of-example.sh
+++ test-validation-of-example.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+xmllint  --schema 'bpmn/BPMN20.xsd' --schema bpmn-envelope-decorator-bounds.xsd --noout bpmn-envelope-decorator-bounds.bpmn
 xmllint  --schema 'bpmn/BPMN20.xsd' --noout bpmn-envelope-decorator-bounds.bpmn
